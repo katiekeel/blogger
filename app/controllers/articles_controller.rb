21a3ebdb99062e993edbe92a1e856f94c5912c48
Katie Shermer <katieshermer@gmail.com>
2017-07-09 14:56:37 -0600

    Added edit functionality, flash messages, partial, root default

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+  before_action :set_article, only: [:destroy, :edit, :update]
+
   def index
     @articles = Article.all
   end
@@ -15,13 +17,24 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.save
+    flash.notice = "Article #{@article.title} created!"
     redirect_to article_path(@article)
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
-    redirect_to action: "index"
+    flash.notice = "Article destroyed!"
+    redirect_to articles_path
+  end
+
+  def edit
+
+  end
+
+  def update
+    @article.update(article_params)
+    flash.notice = "Article '#{@article.title}' Updated!"
+    redirect_to article_path(@article)
   end
 
 
@@ -29,6 +42,10 @@ class ArticlesController < ApplicationController
 
   def article_params
     params.require(:article).permit(:title, :body)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 
 end
